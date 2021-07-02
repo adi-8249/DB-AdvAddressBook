fdcfcc6fc8b4f5f4602385cedcb4345b0e6c31d4
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-02 19:03:51 +0530

    [Anjaneya]Added:Inserting data into the table

--- AdvAdressBook.sql
+++ AdvAdressBook.sql
@@ -14,3 +14,12 @@ zip varchar(10),
 phoneNumber varchar(20),
 emailId varchar(20)
 );
+
+--UC3-Inserting new contact list to the addressbook
+Insert into AddressBook values
+('Anjaneya','Tosh','Nuapatna','Cuttack','Odisha',754035,'+91 8249775974','anjaneya@8249gmail.com'),
+('Pradeep','Pandit','Bindhanima','Cuttack','Odisha',754535,'+91 7064308525','pradeep@8249gmail.com'),
+('Santosh','Sharma','Pankal','Cuttack','Odisha',754030,'+91 8249542102','santosh@8249gmail.com');
+
+--To see the created table
+Select * from  AddressBook; 
