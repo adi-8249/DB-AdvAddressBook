1c8ff50f75ba5e3e7242908049abdc650616b063
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-07-02 18:28:09 +0530

    [Anjaneya]Added:Creating a table and adding attributes

--- AdvAdressBook.sql
+++ AdvAdressBook.sql
@@ -2,3 +2,15 @@
 --UC1-Create databse 
 Create DataBase AddressBookService;
 Use AddressBookService;
+
+--UC2-Creating address book table and adding attributes
+Create table AddressBook(
+firstname varchar(20),
+lastname varchar(20),
+address varchar(50),
+city varchar(20),
+state varchar(10),
+zip varchar(10),
+phoneNumber varchar(20),
+emailId varchar(20)
+);
